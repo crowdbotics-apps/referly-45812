3c5726155f01d5434d66dbfffbafbd274da78cb7
Crowdbotics <team@crowdbotics.com>
2024-02-02 23:06:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "referly-45812",
+  url: "https://referly-45812.botics.co",
+  api: "https://referly-45812.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
